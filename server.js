f0d6915ad7a9dad60fa24c38b9148d445f3b967c
djalmascat <djalmascat@gmail.com>
2020-02-29 01:53:25 -0300

    .

--- server.js
+++ server.js
@@ -8,15 +8,36 @@ server.use(express.static('public'))
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express:server
+    express: server,
+    noCache: true //boolean aceita 2 valores: true or false
 })
+
+
+// Lista de doadores: Vetor ou array
+const donors = [{
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Cleiton Souza",
+        blood: "B+"
+    },
+    {
+        name: "Robson Marques",
+        blood: "O+"
+    },
+    {
+        name: "Mayk Brito",
+        blood: "A-"
+    },
+]
 
 // configurar a apresentação da página
 server.get("/", function (req, res) {
-    return res.render("index.html")
+    return res.render("index.html", { donors })
 })
 
 // ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function () {
-    console.log("iniciei o servidor.")
+    console.log("Server started")
 })
