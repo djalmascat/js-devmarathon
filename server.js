a83b763e2d71d570fc455819f0d8868fef77e092
djalmascat <djalmascat@gmail.com>
2020-02-29 03:24:47 -0300

    .

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const server = express()
 // configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+// habilitar body do formulário
+server.use(express.urlencoded({
+    extended: true
+}))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -33,8 +38,26 @@ const donors = [{
 
 // configurar a apresentação da página
 server.get("/", function (req, res) {
-    return res.render("index.html", { donors })
+    return res.render("index.html", {
+        donors
+    })
 })
+
+server.post("/", function (req, res) {
+    // pegar dados do formulário
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //colocar valores dentro do array
+    donors.push({
+        name: name,
+        blood: blood
+    })
+
+    return res.redirect("/")
+})
+
 
 // ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function () {
