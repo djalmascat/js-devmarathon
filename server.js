ba8a941d959e85d5495f0879cce3c496b7dea989
djalmascat <djalmascat@gmail.com>
2020-02-29 04:14:28 -0300

    .

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ server.use(express.urlencoded({
     extended: true
 }))
 
+// configurar a conexão com o banco de dados
+
+const Pool = require("pg").Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'kasx9906',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -17,29 +28,17 @@ nunjucks.configure("./", {
     noCache: true //boolean aceita 2 valores: true or false
 })
 
-// Lista de doadores: Vetor ou array
-const donors = [{
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Cleiton Souza",
-        blood: "B+"
-    },
-    {
-        name: "Robson Marques",
-        blood: "O+"
-    },
-    {
-        name: "Mayk Brito",
-        blood: "A-"
-    },
-]
 
 // configurar a apresentação da página
 server.get("/", function (req, res) {
-    return res.render("index.html", {
-        donors
+    db.query("SELECT * FROM donors", function (err, result) {
+        if (err) return res.send("Erro de banco de dados")
+
+        const donors = result.rows
+        return res.render("index.html", {
+            donors
+        })
+
     })
 })
 
@@ -49,13 +48,24 @@ server.post("/", function (req, res) {
     const email = req.body.email
     const blood = req.body.blood
 
-    //colocar valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios")
+    }
+
+    //colocar valores dentro do banco de dados
+    const query = `INSERT INTO donors("name", "email", "blood") VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function (err) {
+        // fluxo de erro
+        if (err) return res.send("Erro no banco de dados")
+
+        // fluxo ideal
+        return res.redirect("/")
     })
 
-    return res.redirect("/")
+
 })
 
 
